5aa30d52e1b04063d68d445c66b2d395e5725a81
ZUBO GU <gu.zubo@berkeley.edu>
2018-03-30 02:44:27 -0700

    add home.html and add url path /home

--- firstdjango/scrumboard/urls.py
+++ firstdjango/scrumboard/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from .api import ListApi, CardApi
 
 urlpatterns = [
     path('lists/', ListApi.as_view(), name='lists'),
     path('cards/', CardApi.as_view(), name='cards'),
-
+    path('home/', TemplateView.as_view(template_name="frontend/home.html")),
 ]
